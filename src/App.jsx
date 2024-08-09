651c19a8747ae846c886bfbfe59549370a92041b
Rishu Raj <131179273+Rishut681@users.noreply.github.com>
2024-08-09 19:23:20 +0530

    merge

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import styles from'./App.css';
+// import { About } from "./components/About/About";
+
+function App() {
+
+  return (
+    <div className="App-body">
+
+    </div>
+  )
+}
+
+export default App
